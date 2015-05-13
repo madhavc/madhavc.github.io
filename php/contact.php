1eee890f15e50d0271225436031b566496d4c38a
Madhav Chhura <chhura.madhav@gmail.com>
2015-05-13 01:38:38 -0700

    fixed the contact page

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "me@mchhura.com";
+$address = "chhura.madhav@gmail.com";
 
 
 // Configuration option.
